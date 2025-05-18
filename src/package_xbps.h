2e892f7a078556f556885f7fd279a2a33ff0627d
Igor Botukhov <igor.botukhov@gmail.com>
2025-05-18 20:09:09 +0200

    separate package_xbps

---
+++ src/package_xbps.h
@@ -0,0 +1,19 @@
+#ifndef PACKAGE_XBPS
+#define PACKAGE_XBPS
+
+typedef struct {
+    char* name;
+    char* version;
+} Package;
+
+void install_package_xbps(const char* name);
+
+void removed_package_xbps(const char* name);
+
+void update_packages_xbps();
+
+void collect_dependencies_xbps(const char* name, Package* packages);
+
+int get_all_packages_xbps(Package* packages);
+
+#endif
